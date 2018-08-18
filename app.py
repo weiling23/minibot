2d8e8192e8807eb2eef5e9bb0b704d2754d71a45
weiling23 <40909950+weiling23@users.noreply.github.com>
2018-08-18 16:44:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=(event.message.text)+'哈哈哈')
+        TextSendMessage(text=(event.message.text)+'哈哈哈'))
  
 
 if __name__ == '__main__':
